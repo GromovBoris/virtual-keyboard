2408e3f1ba79b6df034ff05a9fdeddeb9436e816
GromovBoris <drunkgonzo@gmail.com>
2023-04-27 10:58:30 +0300

    docs: html css js files create

---
+++ app.js
@@ -0,0 +1,11 @@
+// Создаем div элемент
+const div = document.createElement("div");
+
+// Добавляем класс к div элементу
+div.classList.add("triangle");
+
+// Добавляем текст в div элемент
+div.textContent = "Привет, мир!";
+
+// Добавляем div элемент в тело документа
+document.body.appendChild(div);
